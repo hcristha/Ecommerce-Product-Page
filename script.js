04a8eabf0cfaf978ff2259dd62eba225cbdb5c8d
Helen Cristhina A <hcristha@outlook.com>
2022-01-28 17:40:12 -0300

    nav
    sombra;
    checar funcionamento

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function showMenu(){
-    let menu = document.getElementsByClassName("js-header__menu")[0];
-    menu.style.display = "block";/*aqui, menu aparecer*/
+    let menu = document.querySelector(".js-menu");
+    menu.style.display = "block";
+
+    let close = document.querySelector(".menu__close");
+    close.addEventListener("click", function(){
+        menu.style.display = "none";
+    })
 }

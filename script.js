d2c073c4be0ed284ad053f6e63228c51aa7ff83c
Helen Cristhina A <hcristha@outlook.com>
2022-01-28 18:39:43 -0300

    menu__box
    ocupar todo body

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function showMenu(){
     let menu = document.querySelector(".js-menu");
+    let box = document.querySelector(".js-menu__box");
     menu.style.display = "block";
+    box.style.display = "block";
 
     let close = document.querySelector(".menu__close");
     close.addEventListener("click", function(){
         menu.style.display = "none";
+        box.style.display = "none";
     })
 }

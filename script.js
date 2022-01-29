204fb1d9d258822029c12b53256cc4b7ac8f4af9
Helen Cristhina A <hcristha@outlook.com>
2022-01-29 15:07:33 -0300

    product__cart
    margin

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+// mostrar menu
 function showMenu(){
     let menu = document.querySelector(".js-menu");
     let box = document.querySelector(".js-menu__box");
     menu.style.display = "block";
     box.style.display = "block";
 
+    // se clicar no "x" dentro do menu, vai fechar menu e box
     let close = document.querySelector(".menu__close");
     close.addEventListener("click", function(){
         menu.style.display = "none";
         box.style.display = "none";
-    })
+    });
+
+    // se clicar fora do menu, ou seja, no box transparente, vai fechar tanto menu quanto box
+    box.addEventListener("click", function(){
+        menu.style.display = "none";
+        box.style.display = "none";
+    });
+}
+
+// mostrar carrinho
+function showCart(){
+    
 }

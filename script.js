f1772bcea627c664dc47d49035d59681bee61e9d
Helen Cristhina A <hcristha@outlook.com>
2022-02-25 16:28:02 -0300

    function cartFilled()
    Bug, em vez de comparar número com número, comparei elemento com número '-'

--- script.js
+++ script.js
@@ -80,7 +80,16 @@ function addCart(){
     itemsCart = amountItem.innerText; // itens do carrinho recebe a quantidade escolhida pelo usuário, se o amountItem atualizar, atualiza também itemsCart na memória
     numberCart.innerText = itemsCart; // atualiza número de itens dentro do cart
     localStorage.setItem("itemsCart", itemsCart); // salva a nova quantidade de itens dentro do cart
-    cartFilled();
+
+    // if (numberCart == 0){
+    //     numberCart.style.display = "none"; // esconder ícone acima do cart
+    // }
+
+    console.log(JSON.parse(itemsCart));
+    if (JSON.parse(itemsCart) !== 0){ // somente executar função de carrinho cheio se 'itemsCart' for diferente de 0
+        console.log(JSON.parse(itemsCart));
+        cartFilled(); // função carrinho cheio
+    }
 }
 
 
@@ -105,13 +114,10 @@ function cartEmpty(){ // função para carrinho vazio
 }
 
 function cartFilled(){ // função para carrinho cheio
-    // AQUI, se o número for 0 não colocar emcima do ícone do carrinho
+    console.log(JSON.parse(numberCart.innerText));
     iconCart.style.fill = "#69707D"; // se carrinho estiver cheio muda para cor cinza
 
-    if (numberCart == 0){
-        numberCart.style.display = "none"; // esconder ícone acima do cart
-    }
-
+    console.log(numberCart); // AQUI! Bug, em vez de comparar número com número, comparei elemento com número '-'
     if (numberCart !== 0){
         numberCart.style.display = "block"; // mostrar ícone acima do cart
     }
